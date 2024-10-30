ff71201cd406dfdfec1155cf978ace2692c70f44
JuanDavidDV <juandadazav@gmail.com>
2024-10-29 22:31:10 -0400

    sort published_at field DESC

--- app/models/blog_post.rb
+++ app/models/blog_post.rb
@@ -2,7 +2,7 @@ class BlogPost < ApplicationRecord
   validates :title, presence: true
   validates :body, presence: true
 
-  scope :sorted, -> { order(published_at: :desc, updated_at: :desc) }
+  scope :sorted, -> { order(arel_table[:published_at].desc.nulls_last).order(updated_at: :desc) }
   scope :draft, -> { where(published_at: nil) } # Has not been set any time for an action
   scope :published, -> { where("published_at <= ?", Time.current) } # Published time is <= to current time
   scope :schedule, -> { where("published_at > ?", Time.current) }
